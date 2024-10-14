2104f3529071faf4e4615fe3282812143afcce8b
olivia-loup <olivia.loup@heig-vd.ch>
2024-10-14 23:04:01 +0200

    test3

---
+++ main.cpp
@@ -0,0 +1,31 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE     500ms
+
+#include "mbed_trace.h"
+#if defined(MBED_CONF_MBED_TRACE_ENABLE)
+#define TRACE_GROUP  "MAIN"
+#endif // MBED_CONF_MBED_TRACE_ENABLE
+
+
+int main()
+{
+    // Initialise the digital pin LED1 as an output
+#ifdef LED1
+    DigitalOut led(LED1);
+#else
+    bool led;
+#endif
+
+    while (true) {
+        led = !led;
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
+}
